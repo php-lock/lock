f121fef5d90731edc7289f2b3209351a50cb6b45
Willem Stuursma-Ruwen <willem@stuursma.name>
2018-04-15 10:23:03 +0200

    Update memcached test

--- tests/mutex/MemcachedMutexTest.php
+++ tests/mutex/MemcachedMutexTest.php
@@ -10,48 +10,34 @@ namespace malkusch\lock\mutex;
  * @author Markus Malkusch <markus@malkusch.de>
  * @link bitcoin:1335STSwu9hST4vcMRppEPgENMHD2r1REK Donations
  * @license WTFPL
+ * @requires extension memcached
  * @see MemcachedMutex
  */
 class MemcachedMutexTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var \Memcached
+     */
+    protected $memcached;
 
     protected function setUp()
     {
-        if (!getenv("MEMCACHE_HOST")) {
-            $this->markTestSkipped();
-            return;
-        }
+        $this->memcached = new \Memcached();
+        $this->memcached->addServer(getenv("MEMCACHE_HOST") ?: "localhost", 11211);
+        $this->memcached->flush();
     }
-    
-    /**
-     * Builds a MemcachedMutex.
-     *
-     * @param string $name The mutex name.
-     * @param \Memcached &$memcache The memcached API.
-     * @param int $timeout The timeout.
-     *
-     * @return MemcachedMutex The Mutex
-     */
-    private static function buildMemcachedMutex($name, &$memcache, $timeout = 3)
-    {
-        if (!getenv("MEMCACHE_HOST")) {
-            return;
-        }
-        $memcache = new \Memcached();
-        $memcache->addServer(getenv("MEMCACHE_HOST"), 11211);
-        return new MemcachedMutex($name, $memcache, $timeout);
-    }
-    
+
     /**
      * Tests failing to acquire the lock.
      *
      * @test
-     * @expectedException malkusch\lock\exception\TimeoutException
+     * @expectedException \malkusch\lock\exception\TimeoutException
      */
     public function testFailAcquireLock()
     {
-        $mutex = self::buildMemcachedMutex("testFailAcquireLock", $memcache, 1);
-        $memcache->add(MemcachedMutex::PREFIX."testFailAcquireLock", true, 2);
+        $mutex = new MemcachedMutex("testFailAcquireLock", $this->memcached, 1);
+
+        $this->memcached->add(MemcachedMutex::PREFIX."testFailAcquireLock", true, 2);
 
         $mutex->synchronized(function () {
             $this->fail("execution is not expected");
@@ -62,14 +48,13 @@ class MemcachedMutexTest extends \PHPUnit_Framework_TestCase
      * Tests failing to release a lock.
      *
      * @test
-     * @expectedException malkusch\lock\exception\LockReleaseException
+     * @expectedException \malkusch\lock\exception\LockReleaseException
      */
     public function testFailReleasingLock()
     {
-        $mutex = self::buildMemcachedMutex("testFailReleasingLock", $memcache);
-
-        $mutex->synchronized(function () use ($memcache) {
-            $memcache->delete(MemcachedMutex::PREFIX."testFailReleasingLock");
+        $mutex = new MemcachedMutex("testFailReleasingLock", $this->memcached, 1);
+        $mutex->synchronized(function () {
+            $this->memcached->delete(MemcachedMutex::PREFIX."testFailReleasingLock");
         });
     }
 }
