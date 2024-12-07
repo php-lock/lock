0ad9a20c07ce73c55d81a86b0464aadbcec64d8a
Michael Voříšek <mvorisek@mvorisek.cz>
2024-12-07 03:03:31 +0100

    drop PhpLockException interface

--- src/Exception/MutexException.php
+++ src/Exception/MutexException.php
@@ -10,7 +10,7 @@ namespace Malkusch\Lock\Exception;
  * Generic exception for any other not covered reason. Usually extended by
  * child classes.
  */
-class MutexException extends \RuntimeException implements PhpLockException
+class MutexException extends \RuntimeException
 {
     /** Not enough redis servers */
     public const CODE_REDLOCK_NOT_ENOUGH_SERVERS = 23786;
